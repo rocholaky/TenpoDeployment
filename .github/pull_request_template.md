9a472dc7174eb629820c726763e05f6b085a9db8
Roberto <roberto.cholaky@ug.uchile.cl>
2025-06-10 22:40:40 -0400

    functional api

---
+++ .github/pull_request_template.md
@@ -0,0 +1,12 @@
+## 🎯 Purpose
+<!-- What does this PR do? -->
+
+## 🛠️ Features
+<!-- List the key changes in this PR -->
+
+
+## 🔍 Proof Screenshots
+<!-- Add screenshots/recordings demonstrating the changes work as expected -->
+
+### For references on emojis in PR titles
+Check: https://gitmoji.dev/
---
+++ api/api_payload.py
@@ -0,0 +1,25 @@
+from typing import List
+
+from pydantic import BaseModel, field_validator
+
+
+class PredictionBody(BaseModel):
+    """
+    Represents the body of a prediction request.
+    """
+
+    inputs: List[float]
+
+    @field_validator("inputs")
+    def validate_inputs(cls, v: list[float]) -> list[float]:
+        """
+        Validate that inputs are a list of floats.
+        """
+
+        if not isinstance(v, list):
+            raise ValueError("Inputs must be a list.")
+        if len(v) == 0:
+            raise ValueError("Inputs cannot be an empty list.")
+        if not all(isinstance(i, float) for i in v):
+            raise ValueError("All inputs must be floats.")
+        return v
